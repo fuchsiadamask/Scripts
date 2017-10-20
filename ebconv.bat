278692e10c20abbfe6ada33d1de506e059adeee8
fuchsiadamask <fuchsiadamask@gmail.com>
2017-10-20 16:47:04 +0300

    minimum line height value set

--- ebconv.bat
+++ ebconv.bat
@@ -24,11 +24,12 @@ echo %defaultColor%
     )
 
     ebook-convert %1 "%~dpn1_%ext%.pdf" --remove-first-image --preserve-cover-aspect-ratio ^
-        --pdf-page-numbers ^
-        --margin-top 38 --margin-bottom 38 --margin-left 52 --margin-right 52 ^
         --paper-size b5 ^
+        --margin-top 38 --margin-bottom 38 --margin-left 52 --margin-right 52 ^
+        --pdf-page-numbers ^
         --pdf-serif-family "PT Serif" --pdf-sans-family "PT Sans" --pdf-mono-family "Cousine" ^
         --pdf-default-font-size 14 --pdf-mono-font-size 12 ^
+        --minimum-line-height 150 ^
         --filter-css font-family,font-size
 
     echo %strongCyan%
